1ac47fad8e22854ed20629abb94a4475e5e647f3
Oleg Zazerin <oleg.zazerin@helmes.com>
2022-06-28 13:47:02 +0300

    Update procfile

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import Configuration from './config/configuration';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log("PORT: ", Configuration().port);
-  await app.listen(Configuration().port);
+  await app.listen(Configuration().port || 8080);
 }
 bootstrap();
